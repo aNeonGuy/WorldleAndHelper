1277c29c749210a19f7d42bd14bce129cd592490
aNeonGuy <150128043+aNeonGuy@users.noreply.github.com>
2025-03-09 17:04:44 +0300

    File with both programs and words

---
+++ WorldleAndHelper/Helper.py
@@ -0,0 +1,34 @@
+print('Hello! Its "Helper" for "Worldle"! / Привет! Это "Помощник" для "Worldle"!')
+print('To help you, say me the number of language(as in "Worldle"): / Чтобы помочь вам, скажите мне номер языка(как в "Worldle"):')
+lang = input()
+if str(lang) == '1':
+    with open('russian.dict', encoding='utf-8') as words:
+        listwords = words.readlines()
+elif str(lang) == '2':
+    with open('english.dict') as words:
+        listwords = words.readlines()
+print('Okay, which length of word you have(tуpe number): / Хорошо, какая длина слова у вас(введите число):')
+length = int(input())
+listwords = filter(lambda word: len(word) == length, listwords)
+answ = 'restart'
+while answ == 'restart':
+    print('Now type letters you have in word(in one line, bettwen put space): / Теперь введите буквы, которые у вас есть в слове(в одну строку через пробел):')
+    listgletts = input().split()
+    for word in listwords:
+        if any(map(lambda lett: lett not in word, listgletts)):
+            listwords.remove(word)
+    print('Now type letters you dont have in word(in one line, bettwen put space): / Теперь введите буквы, которых у вас нет в слове(в одну строку через пробел):')
+    listbletts = input().split()
+    for word in listwords:
+        if any(map(lambda lett: lett in word, listbletts)):
+            listwords.remove(word)
+    print('I sorted for you all words you can have! / Я отсортировал для вас все слова, которые у вас могут быть!')
+    print('Here it is: / Вот они:')
+    print(listwords)
+    print('Do you have any new info? If yes, type "restart" / У вас есть какая-нибудь новая информация? Если да, введите "restart"')
+    print('else anything you want: / иначе все, что захотите:')
+answ = input()
+print('Thank you for using me! / Спасибо за использование!')
+print('Bye! It was the "Helper"!. / Пока! Это был "Helper".')
+print('')
+print('by Neon')
---
+++ WorldleAndHelper/Worldle.py
@@ -0,0 +1,80 @@
+from random import choice as ch
+
+
+print('Hello! / Привет!')
+print('Its a Worldle! / Это Worldle!')
+print('An analogue of Wordly. / Аналог Wordly.')
+print('Worldle has 2 languages: / В Worldle есть 2 языка:')
+print('Russian and English. / Русский и английский.')
+print('You need to guess the word. / Вам нужно угадать слово.')
+print('You type your guess, / Вы вводите свою догадку,')
+print('if some of the letters / если некоторые буквы')
+print('in word on correct possition / в слове на правильной позиции')
+print('you will see them. / Вы их увидите.')
+print('If its not on correct possition / Если они не на правильной позиции')
+print('they will be in special list. / Они будут в специальном списке.')
+print('If this letter not in word / Если этой буквы нет в слове')
+print('it will be in anouther list / она будет в другом списке')
+print('If you success to guess the word / Если вам удастся угадать слово')
+print('in 6 attempts, you win. / за 6 попыток, вы победили.')
+print('Thats it / Вот и все')
+print('Good luck! / Удачи!')
+print('')
+answ = 'VLP'
+while answ != 'Exit':
+    print('Choose language(type number): / Выберите язык(ведите цифру):')
+    print('1 Russian / Русский')
+    print('2 English / Английский')
+    lang = input()
+    word = 'VLP'
+    if str(lang) == '1':
+        with open('russian.dict', encoding='utf-8') as words:
+            word = ch(words.read().split())
+    elif str(lang) == '2':
+        with open('english.dict') as words:
+            word = ch(words.read().split())
+    letts_not_in_word = []
+    letts_in_word = []
+    print(word)
+    length = len(word)
+    emit = '_' * length
+    success = False
+    print('So, thats where game starts! / Здесь начинается игра!')
+    print('Letters you have: / Буквы, которые у вас есть')
+    print(emit)
+    print('Type guess(word): / Введите догадку(слово):')
+    guess = input()
+    for attempts in range(6):
+        for index in range(length):
+            if guess[index] == word[index]:
+                emit_list = list(emit)
+                emit_list[index] = word[index]
+                emit = ''.join(emit_list)
+            if guess[index] in word and word.rfind(guess[index]) != guess[index] and guess[index] not in letts_in_word:
+                letts_in_word.append(guess[index])
+            if guess[index] in word and list(word).count(guess[index]) == list(emit).count(guess[index]):
+                letts_in_word.remove(guess[index])
+            if guess[index] not in word:
+                letts_not_in_word.append(guess[index])
+        if emit == word:
+            success = True
+            break
+        print('letters you have: / Буквы, которые у вас есть')
+        print(emit)
+        print('letters in word not on correct possition: / Буквы на неправельных позициях')
+        print(letts_in_word)
+        print('letters not from word: / Буквы не из слова')
+        print(letts_not_in_word)
+        guess = input()
+    if success:
+        print('You win!!! Congrats! / Вы победили! Поздравляю!')
+    else:
+        print(f'You lost. "{word}" was the correct one. / Вы проиграли. "{word}" было ответом.')
+    print('If you want to play another time / Если хотите ещё сыграть')
+    print('type anything you want / напишите что угодно')
+    print('If not, type "Exit". / Если нет, введите "Exit"')
+    answ = input()
+print('Thank you for using me! / Спасибо за использование!')
+print('Bye! It was the "Worldle". / Пока! Это был "Worldle".')
+print('')
+print('by Neon')
