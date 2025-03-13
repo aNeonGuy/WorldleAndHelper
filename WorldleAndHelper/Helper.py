292c73c7e0efe34eebd625dd84cef10da49658a5
aNeonGuy <150128043+aNeonGuy@users.noreply.github.com>
2025-03-13 21:48:47 +0300

    Update Helper.py

--- WorldleAndHelper/Helper.py
+++ WorldleAndHelper/Helper.py
@@ -1,17 +1,17 @@
-print('Hello! Its "Helper" for "Worldle"! / Привет! Это "Помощник" для "Worldle"!')
-print('To help you, say me the number of language(as in "Worldle"): / Чтобы помочь вам, скажите мне номер языка(как в "Worldle"):')
-lang = input()
-if str(lang) == '1':
-    with open('russian.dict', encoding='utf-8') as words:
-        listwords = words.readlines()
-elif str(lang) == '2':
-    with open('english.dict') as words:
-        listwords = words.readlines()
-print('Okay, which length of word you have(tуpe number): / Хорошо, какая длина слова у вас(введите число):')
-length = int(input())
-listwords = list(filter(lambda word: len(word) == length, listwords))
 answ = 'restart'
 while answ == 'restart':
+    print('Hello! Its "Helper" for "Worldle"! / Привет! Это "Помощник" для "Worldle"!')
+    print('To help you, say me the number of language(as in "Worldle"): / Чтобы помочь вам, скажите мне номер языка(как в "Worldle"):')
+    lang = input()
+    if str(lang) == '1':
+        with open('russian.dict', encoding='utf-8') as words:
+            listwords = words.readlines()
+    elif str(lang) == '2':
+        with open('english.dict') as words:
+            listwords = words.readlines()
+    print('Okay, which length of word you have(tуpe number): / Хорошо, какая длина слова у вас(введите число):')
+    length = int(input())
+    listwords = list(filter(lambda word: len(word) == length, listwords))
     print('Now type letters you have in word(in one line, bettwen put space): / Теперь введите буквы, которые у вас есть в слове(в одну строку через пробел):')
     listgletts = input().split()
     for word in listwords:
@@ -22,9 +22,20 @@ while answ == 'restart':
     for word in listwords:
         if any(map(lambda lett: lett in word, listbletts)):
             listwords.remove(word)
+    print('Now give me mask of the world(__f_d for example): / Теперь дайте мне маску мира (__в_е например):')
+    mask = input()
+    bestwords = []
+    for word in wordlist:
+        check = True
+        word = word.strip()
+        for i in range(len(word)):
+            if word[i] != mask[i] and mask[i] != "_":
+                check = False
+        if check:
+            bestwords.append(word)
     print('I sorted for you all words you can have! / Я отсортировал для вас все слова, которые у вас могут быть!')
     print('Here it is: / Вот они:')
-    print(listwords)
+    print(*bestwords)
     print('Do you have any new info? If yes, type "restart" / У вас есть какая-нибудь новая информация? Если да, введите "restart"')
     print('else anything you want: / иначе все, что захотите:')
 answ = input()
